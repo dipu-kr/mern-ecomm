22e5f7fe997a5317d2836b0f049b79089d8307d1
dipu-kr <dipum906@gmail.com>
2023-11-09 21:38:34 +0530

    added Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{display:'flex',justifyContent:'space-between',padding:'0px 20px'}}>
+        <h3>Logo</h3>
+        <ul style={{display:'flex'}}>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default Header
